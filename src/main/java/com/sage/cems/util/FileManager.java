3c1638c74f7b2f78805b47411fbe14df5248960f
Abdelrhman Mohsen <abdulrahman_20230317@fci.helwan.edu.eg>
2024-11-27 00:05:20 +0200

    Updating files structure again :)

--- src/main/java/com/sage/cems/util/FileManager.java
+++ src/main/java/com/sage/cems/util/FileManager.java
@@ -8,13 +8,13 @@ public interface FileManager {
     * This function return list of records with the searched keyword
     * The record represented as a Map the key is the column header and the value is the data cell
     * */
-    List<Map<ColumnName, String>> getRow(TableName tableName, Object keyWord);
+    List<Map<ColumnName, String>> getRows(TableName tableName, Object keyWord);
 
     // This function update the first record that has the searched keyword with the new record
     void updateRow(TableName tableName, Object keyWord, Map<ColumnName, String> newRow);
 
     // This function insert new raw into the specified table
-    void insertNewRow(TableName tableName, Map<ColumnName, String> newRow);
+    void insertRow(TableName tableName, Map<ColumnName, String> newRow);
 
     // This function delete the first record has the passed keyword
     void deleteRaw(TableName tableName, Object keyWord);
