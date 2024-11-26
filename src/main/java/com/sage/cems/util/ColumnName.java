92a0a24b148fff02bebe981c7852a419d878d037
a7med-3li <154826599+a7med-3li@users.noreply.github.com>
2024-11-27 01:15:05 +0200

    Updating TableName Enum, ColumnName Enum

--- src/main/java/com/sage/cems/util/ColumnName.java
+++ src/main/java/com/sage/cems/util/ColumnName.java
@@ -3,7 +3,30 @@ package com.sage.cems.util;
 public enum ColumnName {
     // Student table
     STUDENT_ID,
-    STUDENT_NAME
-
+    STUDENT_NAME,
+    STUDENT_PHONE_NUMBER,
+    STUDENT_EMAIL,
+    // Lecturer table
+    LECTURER_ID,
+    LECTURER_PHONE_NUMBER,
+    LECTURER_EMAIL,
+    LECTURER_NAME,
+    // Accounts table
+    ACCOUNT_ID,
+    ACCOUNT_PASSWORD,
+    ACCOUNT_ROLE,
+    // Course table
+    COURSE_ID,
+    // Exam table
+    EXAM_ID,
+    EXAM_TYPE,
+    // Solved exams table
+    SOLVED_EXAM_ANSWERS,
+    // Questions
+    QUESTION_INDEX,
+    QUESTION_STATEMENT,
+    QUESTION_CORRECT_ANSWER,
+    QUESTION_TYPE,
+    QUESTION_CHOICES
 
 }
