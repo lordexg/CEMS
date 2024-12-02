491ebe9b4d0eb601c52586bb8798d8e4ff84e772
Abdelrhman Mohsen <abdulrahman_20230317@fci.helwan.edu.eg>
2024-12-02 03:27:06 +0200

    Updating Files to be in the root path of the project

--- src/main/java/com/sage/cems/Configuration.java
+++ src/main/java/com/sage/cems/Configuration.java
@@ -3,6 +3,6 @@ package com.sage.cems;
 import java.io.File;
 
 public class Configuration {
-    public static final String FILES_DIR = System.getProperty("user.home") + File.separator + "Documents" + File.separator + "CEMS";
+    public static final String FILES_DIR = System.getProperty("user.dir") + File.separator + "Files";
 
 }
