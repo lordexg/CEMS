af8df397ea656003909d2581f8aa677e61af00ba
Ahmed Tharwat <ahmedlordexg@gmail.com>
2024-12-11 07:54:47 +0200

    UserDAO fix createMap logic

--- src/main/java/com/sage/cems/daos/UserDAO.java
+++ src/main/java/com/sage/cems/daos/UserDAO.java
@@ -19,47 +19,54 @@ public class UserDAO {
         this.fileManager = fileManager;
     }
 
-    public void updateUser(User user) throws IOException {
-        fileManager.updateRow(TableName.ACCOUNT, createUserMap(user));
-    }
-
     public User getUser(String ID) throws IOException {
-            List<Map<ColumnName, String>> users = fileManager.getRows(TableName.ACCOUNT, ID);
+        List<Map<ColumnName, String>> users = fileManager.getRows(TableName.ACCOUNT, ID);
 
-            if (users.isEmpty()) {
-                return new User();
-            }
+        if (users.isEmpty()) {
+            return new User();
+        }
 
-            Map<ColumnName, String> target = users.getFirst();
+        Map<ColumnName, String> target = users.getFirst();
 
-            User user = new User();
-            user.setID(target.get(ColumnName.ACCOUNT_ID));
-            user.setPassword(target.get(ColumnName.ACCOUNT_PASSWORD));
-            user.setRole(Role.valueOf(target.get(ColumnName.ACCOUNT_ROLE)));
-            return user;
+        User user = new User();
+        user.setID(target.get(ColumnName.ACCOUNT_ID));
+        user.setPassword(target.get(ColumnName.ACCOUNT_PASSWORD));
+        user.setRole(Role.valueOf(target.get(ColumnName.ACCOUNT_ROLE)));
+        return user;
     }
 
     public void addUser(User user) throws IOException {
-        fileManager.insertRow(TableName.ACCOUNT, createUserMap(user));
+        fileManager.insertRow(TableName.ACCOUNT, createUserMapADD(user));
+    }
+
+    public void updateUser(User user) throws IOException {
+        fileManager.updateRow(TableName.ACCOUNT, createUserMapDU(user));
     }
 
     public void deleteUser(User user) throws IOException {
-        fileManager.deleteRow(TableName.ACCOUNT, createUserMap(user));
+        fileManager.deleteRow(TableName.ACCOUNT, createUserMapDU(user));
     }
 
     // this function fills a map(row) from a User object
-    private Map<ColumnName, String> createUserMap(User newUser) throws IOException {
-        Map<ColumnName, String> user = new TreeMap<>();
-
+    private Map<ColumnName, String> createUserMapADD(User newUser) throws IOException {
+        Map<ColumnName, String> userMap = new TreeMap<>();
         // generate new ID
-        String newIDString = EnumHelper.getNewPK(TableName.ACCOUNT);
+        userMap.put(ColumnName.ACCOUNT_ID, EnumHelper.getNewPK(TableName.ACCOUNT));
 
-        user.put(ColumnName.ACCOUNT_ID,newIDString);
-        user.put(ColumnName.ACCOUNT_PASSWORD, newUser.getPassword());
-        user.put(ColumnName.ACCOUNT_ROLE, newUser.getRole().toString());
+        populateUserMap(userMap, newUser);
+        return userMap;
+    }
 
-        return user;
+    private Map<ColumnName, String> createUserMapDU(User newUser) throws IOException {
+        Map<ColumnName, String> userMap = new TreeMap<>();
+        userMap.put(ColumnName.ACCOUNT_ID, newUser.getID());
+        populateUserMap(userMap, newUser);
+        return userMap;
+    }
+
+    // user ID is not here, it's implemented in every createUserMap function
+    private void populateUserMap(Map<ColumnName, String> userMap, User user) throws IOException {
+        userMap.put(ColumnName.ACCOUNT_PASSWORD, user.getPassword());
+        userMap.put(ColumnName.ACCOUNT_ROLE, user.getRole().toString());
     }
 }
-
-
